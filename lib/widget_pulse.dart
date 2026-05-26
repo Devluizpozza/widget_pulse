392d577d047e4e04938a3a385a480839058b34c4
devLuizPozza <luizpozza.2019@gmail.com>
2026-05-26 15:10:21 -0300

    fix: unecessary import

--- lib/widget_pulse.dart
+++ lib/widget_pulse.dart
@@ -1,12 +1,7 @@
-library widget_pulse;
-
-export 'src/widgets/widget_pulse.dart';
-
-export 'src/tracking/tracked_widget.dart';
-export 'src/tracking/pulse_state_mixin.dart';
-
-export 'src/core/pulse_controller.dart';
 export 'src/core/pulse_config.dart';
-
-export 'src/tracking/widget_registry.dart';
+export 'src/core/pulse_controller.dart';
 export 'src/metrics/rebuild_metrics.dart';
+export 'src/tracking/pulse_state_mixin.dart';
+export 'src/tracking/tracked_widget.dart';
+export 'src/tracking/widget_registry.dart';
+export 'src/widgets/widget_pulse.dart';
